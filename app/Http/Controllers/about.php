2fd1939b5498b6b87ec04540554d8f33cbf72e5f
Raj <rajcreation1996@gmail.com>
2018-03-14 00:52:53 +0530

    first pro

---
+++ app/Http/Controllers/about.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class about extends Controller
+{
+ /*public function index()
+ {
+     return view('about-us');
+ }
+
+ */
+ public function raj()
+ {
+     return view('about-us');
+ }
+ public function contact()
+ {
+     return view('contact-us');
+ }
+
+}
